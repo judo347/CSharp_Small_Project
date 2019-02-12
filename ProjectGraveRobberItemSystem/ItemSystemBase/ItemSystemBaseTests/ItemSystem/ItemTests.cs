c4485daef873d416e2b23987353eea0286c42818
Mikkel K <32062449+judo347@users.noreply.github.com>
2019-02-12 10:32:44 +0100

    added ItemTests

---
+++ ProjectGraveRobberItemSystem/ItemSystemBase/ItemSystemBaseTests/ItemSystem/ItemTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ItemSystemBase.ItemSystem;
+
+namespace ItemSystemBaseTests.ItemSystem
+{
+	[TestClass]
+	public class ItemTest
+	{
+		[TestMethod]
+		public void constructor01()
+		{
+			String itemName = "ItemName";
+			int itemId = 5;
+			Item item = new Item(itemName, itemId);
+			Assert.IsTrue(itemName.Equals(item.name));
+			Assert.IsTrue(itemId.Equals(item.id));
+		}
+	}
+}
