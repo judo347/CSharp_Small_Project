0e5c231e168dd4faa031dafd78b68921b1441312
Mikkel Kuntz <flintfedt-trash@hotmail.com>
2019-02-12 09:15:52 +0100

    tests

--- ProjectGraveRobberItemSystem/ItemSystemBase/ItemSystemBase/Program.cs
+++ ProjectGraveRobberItemSystem/ItemSystemBase/ItemSystemBase/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ItemSystemBase.ItemSystem;
 
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("ItemSystemBaseTests")] //This does that the solution ItemSystemBaseTests can see all files in this namespace
 namespace ItemSystemBase
 {
 	class Program
--- ProjectGraveRobberItemSystem/ItemSystemBase/ItemSystemBaseTests/ItemSystem/InventorySlotTest.cs
+++ ProjectGraveRobberItemSystem/ItemSystemBase/ItemSystemBaseTests/ItemSystem/InventorySlotTest.cs
@@ -8,9 +8,77 @@ namespace ItemSystemBaseTests.ItemSystem
 	public class InventorySlotTest
 	{
 		[TestMethod]
+		public void constructor01()
+		{
+			InventorySlot inventorySlot = new InventorySlot();
+			Assert.IsTrue(inventorySlot.isFree);
+		}
+
+		[TestMethod]
+		public void constructor02()
+		{
+			InventorySlot inventorySlot = new InventorySlot();
+			Assert.IsNull(inventorySlot.item);
+		}
+
+		[TestMethod]
 		public void addItem01()
 		{
-			InventorySlot inventorySlot;
+			InventorySlot inventorySlot = new InventorySlot();
+			Item item = new Item("Test Item", 5);
+
+			Boolean addStatus = inventorySlot.addItem(item);
+
+			Assert.IsTrue(addStatus);
+			Assert.IsFalse(inventorySlot.isFree);
+			Assert.IsNotNull(inventorySlot.item);
+		}
+
+		[TestMethod]
+		public void addItem02()
+		{
+			InventorySlot inventorySlot = new InventorySlot();
+			Item item1 = new Item("Test Item", 5);
+			Item item2 = new Item("Test item 2", 3);
+
+			inventorySlot.addItem(item1);
+			Boolean secondAddStatus = inventorySlot.addItem(item2);
+
+			Assert.IsFalse(secondAddStatus);
+			Assert.IsFalse(inventorySlot.isFree);
+			Assert.IsNotNull(inventorySlot.item);
+		}
+
+		[TestMethod]
+		public void popItem01()
+		{
+			InventorySlot inventorySlot = new InventorySlot();
+
+			Item item = inventorySlot.popItem();
+
+			Assert.IsNull(item);
+		}
+
+		[TestMethod]
+		public void popItem02()
+		{
+			InventorySlot inventorySlot = new InventorySlot();
+			String itemName = "Test Item 1";
+			int itemId = 5;
+			Item item1 = new Item(itemName, itemId);
+
+			Boolean addStatus = inventorySlot.addItem(item1);
+
+			Assert.IsTrue(addStatus);
+			Assert.IsNotNull(inventorySlot.item);
+
+			Item poppedItem = inventorySlot.popItem();
+
+			Assert.IsTrue(inventorySlot.isFree);
+			Assert.IsNull(inventorySlot.item);
+			Assert.IsNotNull(poppedItem);
+			Assert.IsTrue(itemName.Equals(poppedItem.name));
+			Assert.IsTrue(itemId.Equals(poppedItem.id));
 		}
 	}
 }
