b001baa79962505110353c279a8afbb0fb1816a0
Mikkel K <32062449+judo347@users.noreply.github.com>
2019-02-12 10:34:35 +0100

    asd

---
+++ ProjectGraveRobberItemSystem/ItemSystemBase/ItemSystemBaseTests/ItemSystem/InventoryTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ItemSystemBaseTests.ItemSystem
+{
+    [TestClass]
+    public class InventoryTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
